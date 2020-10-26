322671fff17aa537f6dd89a8dd7af75866f60d12
kazuhi-ra <kazuhira4126@gmail.com>
2020-10-26 23:32:40 +0900

    現在のwidthを返すカスタムhookを作った

---
+++ hooks/use-get-width.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+const getCurrentWidth = () => {
+  let result = null
+    if (window) {
+      result = window.innerWidth
+    }
+  
+  return result
+}
+
+const useGetWidth = () => {
+  const [current, setCurrent] = useState<number | null>()
+
+  useEffect(() => {
+    if (window) setCurrent(window.innerWidth)
+    let timeout: NodeJS.Timeout | null
+    const onResize = () => {
+      // 500msに一度しかresizeイベントのcallbackを実行しないようにする
+      if (timeout) return
+
+      const width = getCurrentWidth()
+      if (current !== width) {
+        setCurrent(width)
+      }
+
+      timeout = setTimeout(() => (timeout = null), 100)
+    }
+
+    window.addEventListener('resize', onResize)
+
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
+
+  return current
+}
+
+export default useGetWidth
